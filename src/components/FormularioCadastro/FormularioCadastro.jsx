485d11923435f524eb626910bb4c5116254fa005
Danilo Silva <danilo.silva@itenabler.com.br>
2021-05-19 09:45:05 -0300

    Cap 1

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+function FormularioCadastro(){
+  return(
+    <form>
+      <label>Nome</label>
+      <input type="text" />
+
+      <label>Sobrenome</label>
+      <input type="text" />
+
+      <label>CPF</label>
+      <input type="text" />
+
+      <label>Promocoes</label>
+      <input type="checkbox" />
+
+      <label>Novidades</label>
+      <input type="checkbox" />
+
+      <Button type="submit" variant="contained" color="primary">
+        Cadastrar
+      </Button>
+    </form>
+  );
+}
+
+export default FormularioCadastro;
