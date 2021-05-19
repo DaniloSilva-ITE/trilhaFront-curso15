d0b822dabdc808d533e9d3573bc134e533f1ed50
Danilo Silva <danilo.silva@itenabler.com.br>
2021-05-19 10:47:47 -0300

    Cap 3

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { TextField, Button, FormControlLabel, Switch } from '@material-ui/core';
 
 function FormularioCadastro() {
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+
   return (
-    <form>
+    <form 
+      onSubmit={(event) => { 
+        event.preventDefault()
+        console.log(nome, sobrenome); 
+      }}
+    >
       <TextField
+        value={nome}
+        onChange={(event) => {
+          let tmpNome = event.target.value;
+          if(tmpNome.length >= 3){
+            tmpNome = tmpNome.substr(0,3);
+          }
+
+          setNome(tmpNome);
+        }}
         id="nome"
         label="Nome"
         variant="outlined"
@@ -12,6 +29,14 @@ function FormularioCadastro() {
         fullWidth
       />      
       <TextField
+        value={sobrenome}
+        onChange={(event) => {
+            setSobrenome(event.target.value);
+          if(sobrenome.length >= 3){
+            setSobrenome(sobrenome.substr(0,3));
+          }
+        }}
+
         id="sobrenome"
         label="Sobrenome"
         variant="outlined"
@@ -28,13 +53,13 @@ function FormularioCadastro() {
 
       <FormControlLabel
         control={
-          <Switch name="promocoes" defaultChecked="true" color="primary" />
+          <Switch name="promocoes" defaultChecked color="primary" />
         } label="Promoções"
       />
 
       <FormControlLabel
         control={
-          <Switch name="novidades" defaultChecked="true" color="primary" />
+          <Switch name="novidades" defaultChecked color="primary" />
         } label="Novidades"
       />
 
